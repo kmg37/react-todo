a5e258e61fc7001f548a3842133035909343e516
Kendrick <kmg3795@yahoo.com>
2023-09-27 14:04:00 -0500

    mapped objects of array to jsx ul/li

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React from "react";
+
+const todoList = [
+  { title: "This is a title", id: 1 },
+  { title: "Yet another title", id: 2 },
+  { title: "The last title", id: 3 },
+];
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
